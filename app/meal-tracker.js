1a97d224bdcfcfb50b3bece28b4eabe375ca9d4e
Getro Naissance <getro89@gmail.com>
2017-01-22 16:06:36 -0800

    it will add teh tsconfi.json file alongwith typings.json file

--- app/meal-tracker.js
+++ app/meal-tracker.js
@@ -10,4 +10,7 @@ var meals = [];
 meals.push(new Meal('Pizza', 'It was quite delicious', 300));
 meals.push(new Meal('Big-Mac', 'I only ate roughly half', 500));
 meals.push(new Meal('Pop.tarts', 'it was very sugary', 300));
-console.log(meals);
+for (var _i = 0, meals_1 = meals; _i < meals_1.length; _i++) {
+    var meal = meals_1[_i];
+    console.log(meal);
+}
--- app/meal-tracker.ts
+++ app/meal-tracker.ts
@@ -8,4 +8,7 @@ var meals: Meal[] = [];
 meals.push(new Meal('Pizza', 'It was quite delicious', 300));
 meals.push(new Meal('Big-Mac', 'I only ate roughly half', 500));
 meals.push(new Meal('Pop.tarts', 'it was very sugary', 300));
-console.log(meals);
+
+for (var meal of meals) {
+  console.log(meal);
+}
